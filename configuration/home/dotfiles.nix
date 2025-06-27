5db4cf47787a7f63221e6e2bef2fc4e8bb0bc0d6
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-06-28 01:14:42 +0800

    update.

--- configuration/home/dotfiles.nix
+++ configuration/home/dotfiles.nix
@@ -1,13 +1,12 @@
 { config, ... }:
 
 {
-  xdg.configFile = {
-      ".nix-channels".source = ../home/dotfiles/.nix-channels;
-      ".default-wallpaper.png".source = ../home/dotfiles/.default-wallpaper.png;
-      
+  xdg.configFile = {    
       "waybar/config".source = ../home/dotfiles/waybar-config;
       "waybar/style.css".source = ../home/dotfiles/waybar-style.css;
+
       "niri/config.kdl".source = ../home/dotfiles/niri-config.kdl;
+      
       "mako/config".source = ../home/dotfiles/mako-config;
   };
 }
