21d06b1de96f1abdd4f9e45d37a77b6feea3f87a
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-07 23:05:43 +0800

    try to fix java.

--- configuration/services/user/java.nix
+++ configuration/services/user/java.nix
@@ -5,6 +5,7 @@
     package = pkgs.graalvmPackages.graalvm-ce;
   };
 
-  environment.variables._JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd _JAVA_AWT_WM_NONREPARENTING=1";
+  environment.variables._JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=lcd";
+  environment.variables._JAVA_AWT_WM_NONREPARENTING = "1";
 
 }
