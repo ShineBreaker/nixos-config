0c1f6f92672dbc4843e5535e9a33cdf52690dcfa
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-11 16:32:32 +0800

    update.

--- configuration/services/system/fish.nix
+++ configuration/services/system/fish.nix
@@ -20,7 +20,7 @@
       push = "git push";
       rebuild = "sudo nixos-rebuild switch --flake .#";
       rebuild-boot = "sudo nixos-rebuild boot --flake .#";
-      update = "nix flake update && git commit --a -m 'update.' && sudo nixos-rebuild switch --flake .#";
+      update = "nix flake update && git commit -a -m 'update.' && sudo nixos-rebuild switch --flake .#";
       clean = "sudo nix-collect-garbage -d && nix-collect-garbage -d";
       shutdown = "systemctl poweroff";
       reboot = "systemctl reboot";
