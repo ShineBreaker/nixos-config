5730490ab8f3c74dfa1b356bc9e8b3c57fed786e
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-15 20:45:20 +0800

    added nix-ld.

--- configuration/00-main/services.nix
+++ configuration/00-main/services.nix
@@ -11,6 +11,7 @@
     ../services/system/hibernation.nix
     ../services/system/niri.nix
     ../services/system/nix.nix
+    ../services/system/nix-ld.nix
     ../services/system/others.nix
     ../services/system/plymouth.nix
     ../services/system/powerManagement.nix
