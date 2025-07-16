65ee6921377cf18cd87c3ed87e185d448f39f464
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-16 13:51:43 +0800

    reconfiged git.

---
+++ configuration/services/system/git.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, lib, ... }:
+{
+  environment.systemPackages = [
+    pkgs.git-credential-keepassxc
+  ];
+  
+  programs.git = {
+    enable = true;
+    package = pkgs.gitFull;
+    config.credential = {
+      helper = "manager";
+      "https://github.com".username = "ShineBreaker";
+      "https://gitee.com".username = "BrokenShine";
+      credentialStore = "cache";
+    };
+  };
+  
+}
