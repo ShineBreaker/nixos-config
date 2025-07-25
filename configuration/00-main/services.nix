c7d590ccc83a0054fa88af25f25f07eae58800d6
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-25 21:09:16 +0800

    deleted unused.

--- configuration/00-main/services.nix
+++ configuration/00-main/services.nix
@@ -12,7 +12,7 @@
     ../services/system/hibernation.nix
     ../services/system/niri.nix
     ../services/system/nix.nix
-    # ../services/system/nix-ld.nix
+    ../services/system/nix-ld.nix
     ../services/system/others.nix
     ../services/system/plymouth.nix
     ../services/system/powerManagement.nix
@@ -22,7 +22,7 @@
     ../services/user/clash-verge.nix
     ../services/user/fcitx5.nix
     ../services/user/java.nix
-    ../services/user/mako.nix
+    # ../services/user/mako.nix
     ../services/user/poweralertd.nix
     ../services/user/qt6ct.nix
     # ../services/user/sparkle.nix
@@ -36,7 +36,7 @@
   ];
 
   nixpkgs.config.permittedInsecurePackages = [
-
+    # "intel-media-sdk-23.2.2"
   ];
 
 }
