a2917077163ec2d2b597f00cfdae5b047d02e7fc
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-07-28 23:28:29 +0800

    try to fix keyring.

--- configuration/services/system/gnupg.nix
+++ configuration/services/system/gnupg.nix
@@ -1,9 +1,15 @@
-{ config, ... }:
+{ ... }:
 {
-    programs.gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-      enableExtraSocket = true;
-      enableBrowserSocket = true;
-    };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    enableExtraSocket = true;
+    enableBrowserSocket = true;
+  };
+
+  services.gnome.gnome-keyring.enable = true;
+  programs.seahorse.enable = true;
+  security.pam.services.brokenshine.enableGnomeKeyring = true;
+
+  services.passSecretService.enable = true;
 }
