3b87561bb2eb42d825b594b6f9cd5a47935b3b80
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-08-01 22:20:23 +0800

    try to fix kdeconnect.

--- configuration/system/security.nix
+++ configuration/system/security.nix
@@ -15,4 +15,9 @@
   boot.initrd.secrets = {
     "/root.key" = /var/lib/secrets/root.key;
   };
+  
+  networking.firewall = rec {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
