ed48b287dcb04df8dacf41b7a9191254019b4ac3
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-08-23 19:24:22 +0800

    update.

--- configuration/services/system/greetd.nix
+++ configuration/services/system/greetd.nix
@@ -9,7 +9,7 @@
     settings = rec {
       default_session = initial_session;
       initial_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet -r --user-menu --remember-session --time --time-format '%Y-%m-%d %l:%M:%S'";
+        command = "${pkgs.tuigreet}/bin/tuigreet -r --user-menu --remember-session --time --time-format '%Y-%m-%d %l:%M:%S'";
         # command = "${pkgs.niri-unstable}/bin/niri-session";
         user = "brokenshine";
       };
