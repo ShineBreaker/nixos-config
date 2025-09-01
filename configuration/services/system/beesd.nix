fc202abec625491c352ab90f9b0cd89fbdb07eca
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-02 03:05:30 +0800

    added beesd.

--- configuration/services/system/beesd.nix
+++ configuration/services/system/beesd.nix
@@ -10,7 +10,7 @@
   ];
   
   services.beesd.filesystems."default" = {
-    spec = "UUID=eb063fae-136d-476c-9b90-83d1056b513d";
+    spec = "/";
     hashTableSizeMB = 2048;
     verbosity = "crit";
     extraOptions = [ "--loadavg-target" "5.0" "--thread-count" "4" ];
