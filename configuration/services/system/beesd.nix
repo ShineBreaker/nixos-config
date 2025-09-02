4573179371e0c3bdf069dce859913c059b1737c3
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-02 20:01:07 +0800

    modified.

--- configuration/services/system/beesd.nix
+++ configuration/services/system/beesd.nix
@@ -11,7 +11,7 @@
   
   services.beesd.filesystems."default" = {
     spec = "/dev/disk/by-uuid/d8b51e69-ff95-4e60-8373-9581da1d6c7d";
-    hashTableSizeMB = 4096;
+    hashTableSizeMB = 1024;
     verbosity = "crit";
     extraOptions = [ "--loadavg-target" "5.0" "--thread-count" "4" ];
   };
