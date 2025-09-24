1cb9fcf3cd488152aea71b7d14b020733785c22f
BrokenShine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-24 21:13:29 +0800

    update.

--- configuration/home/programs/dankMaterialShell/default.nix
+++ configuration/home/programs/dankMaterialShell/default.nix
@@ -8,28 +8,4 @@
     enable = true;
   };
 
-  services.swayidle = {
-    enable = true;
-    events = [
-      {
-        event = "lock";
-        command = "dms ipc call lock lock";
-      }
-      {
-        event = "unlock";
-        command = "${pkgs.niri}/bin/niri msg action power-on-monitors";
-      }
-      {
-        event = "after-resume";
-        command = "dms ipc call lock lock";
-      }
-    ];
-    timeouts = [
-      {
-        timeout = 600;
-        command = "dms ipc call lock lock && ${pkgs.niri}/bin/niri msg action power-off-monitors";
-      }
-    ];
-  };
-
 }
