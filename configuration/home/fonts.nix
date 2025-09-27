a8e21b47f086b3963d335e4f8c504597f69d64c0
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-27 16:54:46 +0800

    try to fix font.

--- configuration/home/fonts.nix
+++ configuration/home/fonts.nix
@@ -8,15 +8,12 @@
     defaultFonts = {
       serif = [
         "Sarasa Gothic SC"
-        "Iosevka Nerd Font"
       ];
       sansSerif = [
         "Sarasa Gothic SC"
-        "Iosevka Nerd Font"
       ];
       monospace = [
         "Maple Mono NF CN"
-        "Iosevka Nerd Font"
       ];
       emoji = [
         "Noto Color Emoji"
