b99d2c0b78196f08b6c24742c11fbaaa9b868cc9
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-27 18:59:53 +0800

    try to fix fonts.

--- configuration/system/fonts.nix
+++ configuration/system/fonts.nix
@@ -12,10 +12,6 @@
     fontconfig.enable = true;
     fontDir.enable = true;
     packages = with pkgs; [
-      maple-mono.NF-CN
-      nerd-fonts.iosevka
-      noto-fonts-color-emoji
-      noto-fonts-emoji-blob-bin
       sarasa-gothic
     ];
   };
