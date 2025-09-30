cc99e2ed9594735210b683cbf59d3f4b3ba6af58
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-30 14:57:44 +0800

    update

--- configuration/home/programs/firefox.nix
+++ configuration/home/programs/firefox.nix
@@ -50,31 +50,6 @@
      
     profiles.default = {
       isDefault = true;
-      search = {
-        default = "bing";
-        engines = {
-          nix-packages = {
-            name = "Nix Packages";
-            urls = [{
-              template = "https://search.nixos.org/packages";
-              params = [
-                { name = "type"; value = "packages"; }
-                { name = "query"; value = "{searchTerms}"; }
-              ];
-            }];
-
-            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-            definedAliases = [ "@np" ];
-          };
-
-          nixos-wiki = {
-            name = "NixOS Wiki";
-            urls = [{ template = "https://wiki.nixos.org/w/index.php?search={searchTerms}"; }];
-            iconMapObj."16" = "https://wiki.nixos.org/favicon.ico";
-            definedAliases = [ "@nw" ];
-          };
-        };
-      };
     };
   };
 
