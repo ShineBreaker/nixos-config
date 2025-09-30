b624e3c516fcb078af2c87ec83bd6bef720f569f
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-09-30 15:28:34 +0800

    removed addm settings.

--- configuration/services/system/fprintd.nix
+++ configuration/services/system/fprintd.nix
@@ -20,15 +20,4 @@
       auth include    login
     '';
   };
-
-  # security.pam.services.sddm = lib.mkIf (config.services.displayManager.sddm.enable) {
-  #   text = ''
-  #     auth sufficient ${pkgs.linux-pam}/lib/security/pam_unix.so try_first_pass likeauth nullok
-  #     auth sufficient ${pkgs.fprintd}/lib/security/pam_fprintd.so
-  #     auth      substack      login
-  #     account   include       login
-  #     password  substack      login
-  #     session   include       login
-  #   '';
-  # };
 }
