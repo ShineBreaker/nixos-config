70d8f594bd31222311d6b79cebdae672bc548450
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-10-08 19:50:45 +0800

    update.

--- configuration/system/security.nix
+++ configuration/system/security.nix
@@ -12,7 +12,12 @@
 
   networking = {
     nftables.enable = true;
-    firewall.enable = true;
+    
+    firewall = {
+      enable = true;
+      checkReversePath = false;
+    };
+
     hosts."127.0.0.1" = [
       # Block this domain to prevent QQ from auto-updating.
       "qqpatch.gtimg.cn"
