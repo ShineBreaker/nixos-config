b961ccde8feafc53dd8cacbf2d7f5f084f59cde6
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-10-23 20:58:08 +0800

    moved env to system.

--- configuration/home/programs/niri/config.nix
+++ configuration/home/programs/niri/config.nix
@@ -26,26 +26,6 @@
       hide-not-bound = true;
     };
 
-    environment = {
-      CLUTTER_BACKEND = "wayland";
-      GDK_BACKEND = "wayland,x11";
-      MOZ_ENABLE_WAYLAND = "1";
-      NIXOS_OZONE_WL = "1";
-      QT_QPA_PLATFORM = "wayland";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
-      ELECTRON_ENABLE_HARDWARE_ACCELERATION = "1";
-
-      XDG_SESSION_TYPE = "wayland";
-      XDG_CURRENT_DESKTOP = "niri";
-
-      XMODIFIERS = "@im=fcitx";
-      GLFW_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      INPUT_METHOD = "fcitx";
-    };
-
     xwayland-satellite = {
       enable = true;
       path = lib.getExe pkgs.xwayland-satellite-unstable;
--- configuration/services/system/niri.nix
+++ configuration/services/system/niri.nix
@@ -9,17 +9,12 @@
     pwvucontrol
     brightnessctl
 
+    xwayland-satellite-unstable
     wl-clipboard
     libnotify
     cliphist
 
     labwc
-    cage
-
-    xwayland-satellite-unstable
-
-    nemo-with-extensions
-    file-roller
   ];
 
   programs = {
@@ -32,6 +27,27 @@
   };
 
   security.pam.services.gtklock.text = lib.readFile "${pkgs.gtklock}/etc/pam.d/gtklock";
+
+  environment.sessionVariables = {
+    CLUTTER_BACKEND = "wayland";
+    GDK_BACKEND = "wayland,x11";
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # QT_SCALE_FACTOR = "2";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    ELECTRON_ENABLE_HARDWARE_ACCELERATION = "1";
+
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "niri";
+
+    XMODIFIERS = "@im=fcitx";
+    GLFW_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    INPUT_METHOD = "fcitx";
+  };
 
   services = {
     xserver.desktopManager.runXdgAutostartIfNone = true;
