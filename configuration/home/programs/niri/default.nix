9629c3c8adc75d26d53bdd0a183fe8e306f7e330
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-10-25 17:57:49 +0800

    moved variables to home.

--- configuration/home/programs/niri/default.nix
+++ configuration/home/programs/niri/default.nix
@@ -8,4 +8,25 @@
     ./rules.nix
     ./binds.nix
   ];
+
+  home.sessionVariables = {
+    CLUTTER_BACKEND = "wayland";
+    GDK_BACKEND = "wayland,x11";
+    MOZ_ENABLE_WAYLAND = "1";
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = "wayland";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    # QT_SCALE_FACTOR = "2";
+    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    ELECTRON_ENABLE_HARDWARE_ACCELERATION = "1";
+
+    XDG_SESSION_TYPE = "wayland";
+    XDG_CURRENT_DESKTOP = "niri";
+
+    XMODIFIERS = "@im=fcitx";
+    GLFW_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    INPUT_METHOD = "fcitx";
+  };
 }
--- configuration/services/system/niri.nix
+++ configuration/services/system/niri.nix
@@ -28,27 +28,6 @@
 
   security.pam.services.gtklock.text = lib.readFile "${pkgs.gtklock}/etc/pam.d/gtklock";
 
-  environment.sessionVariables = {
-    CLUTTER_BACKEND = "wayland";
-    GDK_BACKEND = "wayland,x11";
-    MOZ_ENABLE_WAYLAND = "1";
-    NIXOS_OZONE_WL = "1";
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    # QT_SCALE_FACTOR = "2";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    ELECTRON_ENABLE_HARDWARE_ACCELERATION = "1";
-
-    XDG_SESSION_TYPE = "wayland";
-    XDG_CURRENT_DESKTOP = "niri";
-
-    XMODIFIERS = "@im=fcitx";
-    GLFW_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    INPUT_METHOD = "fcitx";
-  };
-
   services = {
     xserver.desktopManager.runXdgAutostartIfNone = true;
     gvfs.enable = true;
@@ -58,6 +37,7 @@
   xdg = {
     portal = {
       enable = true;
+      xdgOpenUsePortal = true;
       config = {
         common = {
           default = [ "gnome" ];
