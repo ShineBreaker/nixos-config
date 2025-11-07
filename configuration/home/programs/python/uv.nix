80ce25234696ff39f27dc669102dfe075739a6d4
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-07 20:37:21 +0800

    added uv.fish

--- configuration/home/programs/python/uv.nix
+++ configuration/home/programs/python/uv.nix
@@ -7,8 +7,11 @@
   home.packages = with pkgs; [
     uv
   ];
+  xdg.configFile = {
 
+  };
   xdg.configFile = {
     "uv/uv.toml".source = ./dotfiles/uv/uv.toml;
+    "fish/conf.d/uv.fish".source = ./dotfiles/conda/uv.fish;
   };
 }
