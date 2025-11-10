886f7cf3bde7457ce8d91460393bacdffd3508d6
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-10 18:17:21 +0800

    try to fix swayidle.

--- configuration/home/programs/swayidle.nix
+++ configuration/home/programs/swayidle.nix
@@ -9,11 +9,11 @@
     events = [
       {
         event = "lock";
-        command = "niri msg action power-off-monitors && ${pkgs.gtklock}/bin/gtklock -d";
+        command = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors && ${pkgs.gtklock}/bin/gtklock -d";
       }
       {
         event = "unlock";
-        command = "niri msg action power-on-monitors";
+        command = "${pkgs.niri-unstable}/bin/niri msg action power-on-monitors";
       }
       {
         event = "after-resume";
@@ -23,7 +23,7 @@
     timeouts = [
       {
         timeout = 600;
-        command = "niri msg action power-off-monitors && ${pkgs.gtklock}/bin/gtklock -d";
+        command = "${pkgs.niri-unstable}/bin/niri msg action power-off-monitors && ${pkgs.gtklock}/bin/gtklock -d";
       }
     ];
   };
