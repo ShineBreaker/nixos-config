fdc1374abe684cb0d10be82b457036a461c84d48
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-12 13:07:38 +0800

    changed proton-cachyos.

--- configuration/home/programs/Entertain/00-default.nix
+++ configuration/home/programs/Entertain/00-default.nix
@@ -11,7 +11,7 @@
   home.packages = with pkgs; [
     (heroic.override {
       extraPkgs = pkgs: [
-        pkgs.proton-cachyos_x86_64_v4
+        pkgs.proton-cachyos_x86_64_v3
       ];
     })
 
--- configuration/home/programs/Entertain/lutris.nix
+++ configuration/home/programs/Entertain/lutris.nix
@@ -6,7 +6,7 @@
 {
   programs.lutris = {
     enable = true;
-    defaultWinePackage = pkgs.proton-cachyos_x86_64_v4;
+    defaultWinePackage = pkgs.proton-cachyos_x86_64_v3;
     extraPackages = with pkgs; [
       mangohud
       winetricks
