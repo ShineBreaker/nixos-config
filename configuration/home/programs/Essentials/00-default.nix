79f12a531ac6a03ed15cbb474ee0b224ab83d464
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-13 17:24:08 +0800

    added qq & wechat.

--- configuration/home/programs/Essentials/00-default.nix
+++ configuration/home/programs/Essentials/00-default.nix
@@ -13,10 +13,10 @@
   home.packages = with pkgs; [
     libreoffice
 
-    # (qq.override {
-    #   commandLineArgs = "--enable-wayland-ime --wayland-text-input-version=3";
-    # })
-    # wechat
+    (qq.override {
+      commandLineArgs = "--enable-wayland-ime --wayland-text-input-version=3";
+    })
+    wechat
     telegram-desktop
   ];
 }
