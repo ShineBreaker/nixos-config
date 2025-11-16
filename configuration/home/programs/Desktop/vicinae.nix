c9562ede9349500bf37e8fe646805e915debbe2e
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-16 11:28:08 +0800

    added settings.

--- configuration/home/programs/Desktop/vicinae.nix
+++ configuration/home/programs/Desktop/vicinae.nix
@@ -4,11 +4,42 @@
 }:
 
 {
+  home.packages = with pkgs; [
+    app2unit
+  ];
+
   programs.vicinae = {
     enable = true;
     systemd = {
       enable = true;
       autoStart = true;
+    };
+
+    settings = {
+      closeOnFocusLoss = false;
+      faviconService = "twenty";
+
+      font = {
+        size = 10.5;
+      };
+
+      keybinding = "default";
+      popToRootOnClose = true;
+
+      rootSearch = {
+        searchFiles = false;
+      };
+
+      theme = {
+        iconTheme = "Papirus-Dark";
+        name = "vicinae-dark";
+      };
+
+      window = {
+        csd = true;
+        opacity = 0.98;
+        rounding = 10;
+      };
     };
   };
 }
