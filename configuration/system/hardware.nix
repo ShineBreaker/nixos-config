888d15bd2c253b56a7203046e8cce5ff00567ad7
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-22 21:20:34 +0800

    Update hardware.nix

--- configuration/system/hardware.nix
+++ configuration/system/hardware.nix
@@ -9,8 +9,7 @@
       enable32Bit = true;
     };
 
-    # 只启用必要的固件，而不是所有固件
-    enableAllFirmware = false; # 改为false以避免包含不必要的固件
+    enableAllFirmware = false;
     enableRedistributableFirmware = true;
   };
 }
