bf794462e054df316480eef9267f099ccf5016a7
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-25 10:39:50 +0800

    added timeServers.

--- configuration/system/i18n.nix
+++ configuration/system/i18n.nix
@@ -1,5 +1,5 @@
-{ 
-  ... 
+{
+  ...
 }:
 
 {
@@ -18,4 +18,26 @@
     LC_TELEPHONE = "zh_CN.UTF-8";
     LC_TIME = "zh_CN.UTF-8";
   };
+
+  networking.timeServers = [
+    "0.cn.pool.ntp.org"
+    "1.cn.pool.ntp.org"
+    "2.cn.pool.ntp.org"
+    "3.cn.pool.ntp.org"
+
+    "ntp1.aliyun.com"
+    "ntp2.aliyun.com"
+    "ntp3.aliyun.com"
+
+    "ntp1.ntsc.ac.cn"
+    "ntp2.ntsc.ac.cn"
+    "ntp3.ntsc.ac.cn"
+
+    "ntp1.cstnet.cn"
+    "ntp2.cstnet.cn"
+  ];
+
+  services.htpdate.servers = [
+    "www.baidu.com"
+  ];
 }
