5d29fcbfb487b30923f981d79779425309ea8ffe
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-26 23:22:36 +0800

    configurated libreoffice.

--- configuration/home/programs/Essentials/00-default.nix
+++ configuration/home/programs/Essentials/00-default.nix
@@ -11,7 +11,22 @@
     # ./wps-office/default.nix
   ];
   home.packages = with pkgs; [
-    libreoffice
+    (libreoffice.overrideAttrs {
+      variant = "fresh";
+      withHelp = false;
+      kdeIntegration = false;
+      withJava = false;
+
+      langs = [
+        "en-GB"
+        "en-US"
+        "zh-CN"
+      ];
+
+      noto-fonts = null;
+      noto-fonts-lgc-plus = null;
+      noto-fonts-cjk-sans = null;
+    })
     obsidian
 
     (qq.override {
