6220ccf7e7af7630dc46afc5e707e4d7bf7433c4
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-28 17:35:09 +0800

    removed sth.

--- configuration/home/desktopEntries.nix
+++ configuration/home/desktopEntries.nix
@@ -6,25 +6,10 @@
 {
   xdg = {
     desktopEntries = {
-
-      # qq = {
-      #   name = "QQ";
-      #   exec = "${pkgs.qq}/bin/qq --enable-wayland-ime --wayland-text-input-version=3 %U";
-      #   icon = "${pkgs.qq}/share/icons/hicolor/512x512/apps/qq.png";
-      #   categories = [ "Network" ];
-      #   comment = "轻松做自己";
-      # };
-
       labwc = {
         name = "labwc";
         exec = "${pkgs.labwc}/bin/labwc";
       };
-
-      mcaselector = {
-        name = "mcaselector";
-        exec = "mcaselector";
-      };
-
     };
   };
 }
