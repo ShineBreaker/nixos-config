8d53a5be916c32c3aec83d270019c0e7aa405d6d
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-11-29 19:20:51 +0800

    update.

--- configuration/home/programs/Utility/winapps/default.nix
+++ configuration/home/programs/Utility/winapps/default.nix
@@ -8,7 +8,9 @@
     "winapps/winapps.conf".source = ./dotfiles/winapps.conf;
     "winapps/compose.yaml".source = ./dotfiles/compose.yaml;
   };
+
   home.packages = with pkgs; [
     freerdp
   ];
+
 }
