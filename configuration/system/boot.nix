878c0c39fea2a379b3618f68f4c66d5de3e13c6e
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-12-03 21:27:19 +0800

    removed v4l2loopback.

--- configuration/system/boot.nix
+++ configuration/system/boot.nix
@@ -45,14 +45,6 @@
       cleanOnBoot = true;
     };
 
-    extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback
-    ];
-
-    extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=2 exclusive_caps=1 card_label="OBS Virtual Camera"
-    '';
-
     supportedFilesystems = {
       btrfs = true;
       ext4 = true;
