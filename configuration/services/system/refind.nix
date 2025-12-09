fef8fd26bd94a382aa134fd1a0d1a69dd45dbf36
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-12-09 18:43:13 +0800

    try to fix refind.

--- configuration/services/system/refind.nix
+++ configuration/services/system/refind.nix
@@ -1,14 +1,22 @@
 {
+  lib,
   pkgs,
   ...
 }:
 
 {
-  boot.loader.refind = {
-    enable = true;
-    maxGenerations = 10;
-    additionalFiles = {
-      "efi/memtest86/memtest86.efi" = "${pkgs.memtest86-efi}/BOOTX64.efi";
+  boot.loader = {
+    systemd-boot.enable = lib.mkForce false;
+    grub.enable = false;
+    refind = {
+      enable = true;
+      maxGenerations = 10;
+      additionalFiles = {
+        "efi/memtest86/memtest86.efi" = "${pkgs.memtest86-efi}/BOOTX64.efi";
+      };
+      extraConfig = ''
+        include themes/theme.conf
+      '';
     };
   };
 }
