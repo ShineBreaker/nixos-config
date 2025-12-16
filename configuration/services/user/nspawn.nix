8f397c1f2fd2ce2231eac56bd49d03a668e1ddf4
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-12-16 20:16:43 +0800

    added nspawn.

---
+++ configuration/services/user/nspawn.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  ...
+}:
+
+{
+  systemd.targets.machines.enable = true;
+  systemd.nspawn."archlinux" = {
+    enable = true;
+    execConfig = {
+      Boot = true;
+    };
+
+    filesConfig = {
+      # Bind resolve.conf to get networking
+      BindReadOnly = [ "/etc/resolv.conf:/etc/resolv.conf" ];
+      # Bind any directories that you want to be shared
+      Bind = [ "${config.xdg.dataHome}/archlinux" ];
+    };
+    networkConfig = {
+      Private = false;
+    };
+  };
+  systemd.services."systemd-nspawn@archlinux" = {
+    enable = true;
+    requiredBy = [ "machines.target" ];
+    overrideStrategy = "asDropin";
+  };
+}
