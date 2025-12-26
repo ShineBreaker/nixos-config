e83316c9fe8fc6824650fa4a3deb75f398e19b04
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2025-12-26 10:57:24 +0800

    changed table size.

--- configuration/services/system/beesd.nix
+++ configuration/services/system/beesd.nix
@@ -1,18 +1,22 @@
-{ 
+{
   pkgs,
   ...
 }:
-
 
 {
   environment.systemPackages = [
     pkgs.bees
   ];
-  
+
   services.beesd.filesystems."default" = {
     spec = "/dev/disk/by-uuid/2f6b6499-2a27-4583-847f-dd70b9fcb036";
-    hashTableSizeMB = 1024;
+    hashTableSizeMB = 128;
     verbosity = "crit";
-    extraOptions = [ "--loadavg-target" "5.0" "--thread-count" "4" ];
+    extraOptions = [
+      "--loadavg-target"
+      "5.0"
+      "--thread-count"
+      "4"
+    ];
   };
 }
