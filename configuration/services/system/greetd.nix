569a28351d5c576d35d7311cb70178685296b1ec
brokenshine <9560404+brokenshine@user.noreply.gitee.com>
2026-01-07 23:07:41 +0800

    removed niri-unstable.

--- configuration/services/system/greetd.nix
+++ configuration/services/system/greetd.nix
@@ -10,7 +10,7 @@
       default_session = initial_session;
       initial_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet -r --user-menu --remember-session --time --time-format '%Y-%m-%d %l:%M:%S'";
-        # command = "${pkgs.niri-unstable}/bin/niri-session";
+        # command = "${pkgs.niri}/bin/niri-session";
         user = "brokenshine";
       };
     };
