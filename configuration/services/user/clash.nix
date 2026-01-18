ff4b0cf0fa1a02d4be0544fa3083d0d49dc3c00a
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-18 16:37:55 +0800

    Update clash.nix

--- configuration/services/user/clash.nix
+++ configuration/services/user/clash.nix
@@ -1,34 +1,15 @@
 {
-  pkgs,
-  lib,
   ...
 }:
 
 {
-
-  programs.clash-verge = {
+  services.mihomo = {
     enable = true;
-    package = pkgs.clash-verge-rev;
+    configFile = "/home/brokenshine/.config/mihomo/config.yaml";
     tunMode = true;
-    serviceMode = true;
   };
 
-  networking.proxy.default = "http://127.0.0.1:7897";
-
-  /*
-    environment.systemPackages = [
-      pkgs.sparkle
-    ];
-
-    security.wrappers.tunnel = {
-      owner = "root";
-      group = "root";
-      capabilities = "cap_net_bind_service,cap_net_raw,cap_net_admin=+ep";
-      source = lib.getExe pkgs.sparkle;
-    };
-
-    networking.proxy.default = "http://127.0.0.1:7890";
-  */
+  networking.proxy.default = "http://127.0.0.1:7890";
 
   networking.firewall.trustedInterfaces = [ "mihomo" ];
 }
