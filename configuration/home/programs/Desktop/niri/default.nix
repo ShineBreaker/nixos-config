6e7c8efb9732b89de8f664b226ab093883284930
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-29 18:38:09 +0800

    try to fix it.

--- configuration/home/programs/Desktop/niri/default.nix
+++ configuration/home/programs/Desktop/niri/default.nix
@@ -5,6 +5,6 @@
 {
   xdg.configFile = {
     "niri/config.kdl".source = ./dotfiles/config.kdl;
-    "niri/custom.kdl".source = ./dotfiles/custom.kdl;
+    # "niri/custom.kdl".source = ./dotfiles/custom.kdl;
   };
 }
