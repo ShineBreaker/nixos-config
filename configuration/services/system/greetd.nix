fd75c365cbcbbca676e61113e527e1eacf6b4d89
BrokenShine <brokenshine@noreply.codeberg.org>
2026-02-12 22:30:28 +0800

    fixed greetd.

--- configuration/services/system/greetd.nix
+++ configuration/services/system/greetd.nix
@@ -6,11 +6,13 @@
 {
   services.greetd = {
     enable = true;
-    settings = rec {
-      default_session = initial_session;
-      initial_session = {
+    settings = {
+      default_session = {
         command = "${pkgs.tuigreet}/bin/tuigreet -r --user-menu --remember-session --time --time-format '%Y-%m-%d %l:%M:%S'";
-        # command = "${pkgs.niri}/bin/niri-session";
+        user = "brokenshine";
+      };
+      initial_session = {
+        command = "${pkgs.niri}/bin/niri-session";
         user = "brokenshine";
       };
     };
