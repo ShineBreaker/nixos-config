6ee5b547235ff64cbab34bc99d0408adf688ac55
BrokenShine <brokenshine@noreply.codeberg.org>
2026-02-12 22:31:28 +0800

    using waybar.

--- configuration/home/programs/Desktop/00-default.nix
+++ configuration/home/programs/Desktop/00-default.nix
@@ -8,15 +8,15 @@
     # ./dankMaterialShell/default.nix
     # ./linux-wallpaperengine/default.nix
     ./niri/default.nix
-    ./noctalia/default.nix
+    # ./noctalia/default.nix
     ./rime/default.nix
-    # ./waybar/default.nix
+    ./waybar/default.nix
 
     # ./fuzzel.nix
-    # ./mako.nix
-    # ./swayidle.nix
-    # ./vicinae.nix
-    # ./waypaper.nix
+    ./mako.nix
+    ./swayidle.nix
+    ./vicinae.nix
+    ./waypaper.nix
   ];
 
   home.packages = with pkgs; [
